8809df5b560ffc8e3e73f344d992e48c18b93fed
с <n-armstrong@mail.ru>
2026-02-10 01:00:14 +0300

    Transfer to facade all

--- api_mailhog/apis/mailhog_api.py
+++ api_mailhog/apis/mailhog_api.py
@@ -1,4 +1,3 @@
-import requests
 from restclient.client import RestClient
 
 
--- tests/functional/put_v1_account_token/test_put_v1_account_token.py
+++ tests/functional/put_v1_account_token/test_put_v1_account_token.py
@@ -1,11 +1,4 @@
-import pprint
-from json import loads
-
-from dm_api_account.apis.account_api import AccountApi
-from dm_api_account.apis.login_api import LoginApi
-from api_mailhog.apis.mailhog_api import MailhogApi
 from helpers.account_helper import AccountHelper
-
 from restclient.configuration import Configuration as MailhogConfiguration
 from restclient.configuration import Configuration as DmApiConfiguration
 from services.api_mailhog import MailHogApi
