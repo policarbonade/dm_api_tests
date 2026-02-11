a039085e0386ec4c207d897c8903d98cde74f6b6
с <n-armstrong@mail.ru>
2026-02-12 00:03:50 +0300

    Fix test_post_v1_account.py

--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -1,32 +1,7 @@
-import structlog
-from restclient.configuration import Configuration as DmApiConfiguration
-from services.dm_api_account import DmApiAccount
+def test_post_v1_account(account_helper, prepare_user):
+    login = prepare_user.login
+    password = prepare_user.password
+    email = prepare_user.email
+    response = account_helper.register_user(login=login, password=password, email=email)
+    account_helper.user_login(login=login, password=password)
 
-structlog.configure(
-    processors=[
-        structlog.processors.JSONRenderer(
-            indent=4,
-            ensure_ascii=True,
-            sort_keys=True
-        )
-    ]
-)
-
-
-def test_post_v1_account():
-    dm_api_configuration = DmApiConfiguration(host='http://185.185.143.231:5051')
-
-    account = DmApiAccount(configuration=dm_api_configuration)
-
-    # Регистрация пользователя
-    login = 'polinad73'
-    password = '123456789'
-    email = f'{login}@mail.ru'
-    json_data = {
-        'login': login,
-        'email': email,
-        'password': password,
-    }
-
-    response = account.account_api.post_v1_account(json_data=json_data)
-    assert response.status_code == 201, f"Пользователь не создан, {response.json()}"
