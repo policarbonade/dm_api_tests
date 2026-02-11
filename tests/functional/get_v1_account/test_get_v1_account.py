f539161fd1326ca84c4c079506793f13dee46034
с <n-armstrong@mail.ru>
2026-02-12 00:09:38 +0300

    Fix test_get_v1_account.py

--- tests/functional/get_v1_account/test_get_v1_account.py
+++ tests/functional/get_v1_account/test_get_v1_account.py
@@ -1,6 +1,8 @@
 def test_get_v1_account_auth(auth_account_helper):
-    auth_account_helper.dm_account_api.account_api.get_v1_account()
+    response = auth_account_helper.dm_account_api.account_api.get_v1_account()
+    assert response.status_code == 200, "Authorized user is not retrieved"
 
 
 def test_get_v1_account_no_auth(account_helper):
-    account_helper.dm_account_api.account_api.get_v1_account()
+    response = account_helper.dm_account_api.account_api.get_v1_account()
+    assert response.status_code == 200, "Unauthorized user is not retrieved"
