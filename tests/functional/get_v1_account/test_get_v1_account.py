f7d6eccce8219dd802a3890e96ed5d292b6ff4ee
с <n-armstrong@mail.ru>
2026-02-21 21:38:46 +0300

    Soft Assertions add

--- tests/functional/get_v1_account/test_get_v1_account.py
+++ tests/functional/get_v1_account/test_get_v1_account.py
@@ -1,7 +1,9 @@
 from checkers.http_checkers import check_status_code_http
 from datetime import datetime
+from assertpy import assert_that, soft_assertions
+from models.user_envelope import UserRole
 from hamcrest import (
-    assert_that,
+    assert_that as hamcrest_that,
     has_property,
     empty,
     is_not,
@@ -19,7 +21,7 @@ def test_get_v1_account_auth(auth_account_helper):
     """
     with check_status_code_http("", 200):
         response = auth_account_helper.get_account(is_validated=True)
-        assert_that(
+        hamcrest_that(
             response, all_of(
                 has_property("resource", has_properties({
                     "info": equal_to(""),
@@ -35,6 +37,11 @@ def test_get_v1_account_auth(auth_account_helper):
                 }))
             )
         )
+
+    with soft_assertions():
+        assert_that(response.resource.login).is_equal_to("joepeach9")
+        assert_that(response.resource.roles).contains(UserRole.GUEST, UserRole.PLAYER)
+        assert_that(response.resource.online).is_instance_of(datetime)
 
 
 def test_get_v1_account_no_auth(account_helper):
