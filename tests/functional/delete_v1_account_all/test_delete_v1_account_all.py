dd9cc267abf5439c7cd2fb76fd70295086c01c22
с <n-armstrong@mail.ru>
2026-03-01 13:32:48 +0300

    Telegram notifications debug

--- tests/functional/delete_v1_account_all/test_delete_v1_account_all.py
+++ tests/functional/delete_v1_account_all/test_delete_v1_account_all.py
@@ -1,9 +1,6 @@
 import allure
-import os
-
 
 
 @allure.title("Тест на проверку логаута пользователя")
 def test_logout(auth_account_helper):
-    print(f'ТЕКУЩАЯ ДИРЕКТОРИЯ {os.getcwd()}')
     auth_account_helper.logout_all()
